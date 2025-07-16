a061f38c3c31378887214e0945cfae392fb27734
Gabriel Batista <gabrielbatista@MacBook-Air-de-admin.local>
2025-07-16 16:01:45 -0300

    Porquinho_de_dinheiro em progresso

---
+++ porquinho_de_dinheiro.py
@@ -0,0 +1,8 @@
+#region Porquinho de dinheiro
+def fib(n):
+    dp = [0, 1] + [0] * (n - 1)
+    for i in range(2, n + 1):
+        dp[i] = dp[i-1] + dp[i-2]
+    return dp[n]
+print(fib(10000))
+#endregion
