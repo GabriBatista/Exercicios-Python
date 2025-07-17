4768a7b81f02fff41ff68a53c43ab931bf332003
Gabriel Batista <gabrielbatista@MacBook-Air-de-admin.local>
2025-07-17 16:08:59 -0300

    Porquinho de dinheiro em progresso

--- porquinho_de_dinheiro.py
+++ porquinho_de_dinheiro.py
@@ -1,8 +1,9 @@
 #region Porquinho de dinheiro
-def fib(n):
-    dp = [0, 1] + [0] * (n - 1)
-    for i in range(2, n + 1):
-        dp[i] = dp[i-1] + dp[i-2]
-    return dp[n]
-print(fib(10000))
+casos = int(input('Dígite quantos casos devem ser realizados\n'))
+for i in range(casos):
+    x, y = map(int, input(f'Escreva o peso do porquinho vazio, e cheio de moedas, nessa ordem (Caso {i + 1}º)\n').split())
+    diferenca = abs(x - y)
+    moedas = int(input('Quantos tipos de moedas você tem?\n'))
+    for j in range(moedas):
+        valorMoeda, pesoMoeda = map(int, input(f'Tipo {j + 1}\n').split())
 #endregion
